00a9d87c11c7ed2cfc38807af6f14e7a9cc7a330
Arvind Kaushik <kaushik.arvind@gmail.com>
2020-11-29 01:08:18 +0000

    updated pipeline file

--- main.tf
+++ main.tf
@@ -1,31 +1,45 @@
 provider "azurerm" {
-  version = "2.5.0"
-  features {}
+    version = "2.5.0"
+    features {}
+}
+# creating persitance storage for terraform state files
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore"
+        storage_account_name = "tfstoragekaushikarvind"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
 }
 
 resource "azurerm_resource_group" "tf_test" {
-  name     = "tfmainrg"
+  name = "tfmainrg"
   location = "UK South"
 }
 
 resource "azurerm_container_group" "tfcg_test" {
-  name                = "weatherapi"
-  location            = azurerm_resource_group.tf_test.location
-  resource_group_name = azurerm_resource_group.tf_test.name
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
 
-  ip_address_type = "public"
-  dns_name_label  = "kaushikarvinda"
-  os_type         = "Linux"
+  ip_address_type     = "public"
+  dns_name_label      = "kaushikarvinduks"
+  os_type             = "Linux"
 
   container {
-    name   = "weatherapi"
-    image  = "kaushikarvind/weatherapi"
-    cpu    = "1"
-    memory = "1"
+      name            = "weatherapi"
+      image           = "kaushikarvind/weatherapi:${var.imagebuild}"
+        cpu             = "1"
+        memory          = "1"
 
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
   }
 }
